2aa4a9b820263780b8ca850336f22f230c181c1f
Rufii55 <arufianantanes@gmail.com>
2024-05-16 14:44:34 +0200

    Capa visual mostrar

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.empresa.javafxmongocurd100 {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires mongo.java.driver;
 
     opens com.empresa.javafxmongocurd100 to javafx.fxml;
     exports com.empresa.javafxmongocurd100;
